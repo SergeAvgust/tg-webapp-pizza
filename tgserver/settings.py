83b70046e18d4915870b111d978979d9475db472
SergeAvgust <avg89537878078@gmail.com>
2023-03-19 11:32:31 +0300

    Blank settings

--- tgserver/settings.py
+++ tgserver/settings.py
@@ -3,7 +3,7 @@ import yaml
 
 BASE_DIR = pathlib.Path(__file__).parent.parent
 config_path = BASE_DIR / 'config' / 'tgserver.yaml'
-TG_TOKEN = '5738040826:AAE6-BIugfiuTPRHgipGq7NVD1ys0e9olRY'
+TG_TOKEN = 'TOKEN'
 
 def get_config(path):
     with open(path) as f:
