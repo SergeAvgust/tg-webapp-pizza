526eb4842d389d1d85b80ebb036e4c76b00369d5
SergeAvgust <avg89537878078@gmail.com>
2023-03-19 13:41:26 +0300

    Move token to config file

--- tgserver/settings.py
+++ tgserver/settings.py
@@ -3,7 +3,7 @@ import yaml
 
 BASE_DIR = pathlib.Path(__file__).parent.parent
 config_path = BASE_DIR / 'config' / 'tgserver.yaml'
-TG_TOKEN = 'TOKEN'
+
 
 def get_config(path):
     with open(path) as f:
@@ -11,3 +11,4 @@ def get_config(path):
     return config
 
 config = get_config(config_path)
+TG_TOKEN = config['TG_TOKEN']
