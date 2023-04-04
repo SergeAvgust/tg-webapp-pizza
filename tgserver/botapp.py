6de4018e3b0dbe56e359c4ac01864e207cd63e97
SergeAvgust <avg89537878078@gmail.com>
2023-04-04 14:26:27 +0300

    Setup WebApp js script and launch handlers

--- tgserver/botapp.py
+++ tgserver/botapp.py
@@ -1,10 +1,26 @@
 from settings import TG_TOKEN
-from telegram import Update
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, WebAppInfo
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
+import json
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message with a button that opens a the web app."""
+    await update.message.reply_text(
+        "Please press the button below to choose a color via the WebApp.",
+        reply_markup=ReplyKeyboardMarkup.from_button(
+            KeyboardButton(
+                text="Open the color picker!",
+                web_app=WebAppInfo(url="https://sergeavgust.github.io/tg-webapp-bot/"),
+            ),
+            resize_keyboard=True
+        ),
+        
+    )
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+async def web_app_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Print the received data and remove the button."""
+    data = json.loads(update.effective_message.web_app_data.data)
+    await update.message.reply_html(text=data)
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
@@ -13,8 +29,10 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 bot = ApplicationBuilder().token(TG_TOKEN).build()
     
 start_handler = CommandHandler('start', start)
+web_app_data_handler = MessageHandler(filters.StatusUpdate.WEB_APP_DATA, web_app_data)
 echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
 bot.add_handler(start_handler)
+bot.add_handler(web_app_data_handler)
 bot.add_handler(echo_handler)
 
 async def bot_setup(args):
--- tgserver/static/app.js
+++ tgserver/static/app.js
@@ -2,17 +2,20 @@ let tg = window.Telegram.WebApp;
 
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
+tg.MainButton.hide();
+
 
 let order = {};
 let pizzas = {};
 let money = 0;
 
-order['user'] = tg.initDataUnsafe.user.username
-
 function incrementSum(amount) {
-    money += amount;
+    parseInt(money)
+    parseInt(amount)
+    money += parseInt(amount);
+    m = money.toString()
     if (money > 0) {
-        tg.MainButton.setText(money);
+        tg.MainButton.setText(m);
         tg.MainButton.show();
     }
     else {
@@ -33,28 +36,30 @@ inner_btns.forEach(btn_div => {
         counter++;
         counter_field.innerHTML = counter;
         pizzas[btn_div.id] = counter;
-        incrementSum(btn_div.querySelector('.price').value)
+        p = btn_div.querySelector('.price')
+        v = p.getAttribute('data-value')
+        incrementSum(v)
     });
 
     rmv_btn.addEventListener('click', () => {
-        if (counter > 1) {
+        if (counter > 0) {
             counter--;
             counter_field.innerHTML = counter;
             pizzas[btn_div.id] = counter;
-            incrementSum(-btn_div.querySelector('.price').value)
+            incrementSum(-parseInt(btn_div.querySelector('.price').getAttribute('data-value')))
+            if (counter === 0) {delete pizzas[btn_div.id]; }
         }
-        else if (counter === 1) {
-            counter--;
-            counter_field.innerHTML = counter;
-            delete pizzas[btn_div.id];
-            incrementSum(-btn_div.querySelector('.price').value)
-        }
+        
     });
 
 
 });
 
-Telegram.WebApp.onEvent('MainButtonClicked', () => {
+tg.MainButton.onClick(function(){
+    tg.MainButton.setText('Clicked via onClick');
     order['pizzas'] = pizzas;
-    tg.sendData(order)
+
+    tg.sendData(JSON.stringify(order));
+    tg.MainButton.setText('data passed to bot');
+    tg.close();
 })
